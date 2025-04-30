bb9bf02d15141491f1e36253f70d1ce7bd6d538d
aestdile@gmail.com <aestdile@gmail.com>
2025-04-30 07:09:09 +0500

    Add project files.

---
+++ BelissimoPizzaBot/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BelissimoPizzaBot;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the 'Belissimo Pizza Bot'");
+    }
+}
+
+
